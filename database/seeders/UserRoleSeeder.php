778860bed9a5181ecb71dbfa78c78d380eb0105f
Arbaniasfi18 <arbanidalimunthe@gmail.com>
2024-06-10 18:32:09 +0700

    feat(#1) : add another user and his role

--- database/seeders/UserRoleSeeder.php
+++ database/seeders/UserRoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserRoleSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class UserRoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'user_id' => 1,
+                'role_id' => 1,
+            ],
+            [
+                'user_id' => 2,
+                'role_id' => 2,
+            ],
+        ];
+        DB::table('user_roles')->insert($data);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -14,11 +15,20 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            "name" => "Arbani",
-            "email" => "arbani@gmail.com",
-            "phone_num" => "08116790801",
-            "password" => "arbani",
+            [
+                "name" => "Arbani",
+                "email" => "arbani@gmail.com",
+                "phone_num" => "08116790801",
+                "password" => "arbani",
+            ],
+            [
+                "name" => "daud",
+                "email" => "daud@gmail.com",
+                "phone_num" => "08116790801",
+                "password" => "daud",
+            ],
         ];
-        User::create($data);
+        DB::table('users')->insert($data);
+        // User::create($data);
     }
 }
